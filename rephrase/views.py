510b626798b0bd84502ffae6dbc7637ce4159e1b
JesseFentress <jessefentress246@gmail.com>
2021-11-13 18:22:06 -0500

    suggested friends with graph working?

--- rephrase/views.py
+++ rephrase/views.py
@@ -3,11 +3,16 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.forms import AuthenticationForm
 from django.http import HttpResponse
+from rephrase.forms import UserRegistrationForm, EditUserForm, AddFriendForm
+import json
+from rephrase.Graph import Graph
 from rephrase.forms import UserRegistrationForm, EditUserForm
 from rephrase.models import Chat, Message, User
 
-
 # Create your views here.
+from rephrase.models import FriendsList, User
+
+
 def home(request):
     return render(request, 'home.html')
 
@@ -40,7 +45,6 @@ def user_login(request):
             username = form.cleaned_data.get('username')
             password = form.cleaned_data.get('password')
             user = authenticate(request, username=username, password=password)
-
             if user is not None:
                 login(request, user)
                 return redirect('account')
@@ -56,7 +60,46 @@ def user_logout(request):
 
 
 def account(request):
-    return render(request, "account.html")
+    context = {}
+    curr_user = request.user
+    try:
+        friend_list = FriendsList.objects.get(user=curr_user)
+    except FriendsList.DoesNotExist:
+        friend_list = FriendsList(user=curr_user)
+        friend_list.save()
+    if curr_user:
+        friends = friend_list.friends.all()
+        if friends:
+            suggested_friends_dict = {User.objects.get(username=curr_user.username): []}
+            for friend in friends:
+                friends_friends = FriendsList.objects.get(user=friend)
+                suggested_friends = friends_friends.friends.all()
+                my_friend = User.objects.get(username=friend)
+                if suggested_friends is not None:
+                    for suggestion in suggested_friends:
+                        friend_x = User.objects.get(username=suggestion)
+                        if my_friend in suggested_friends_dict.keys():
+                            suggested_friends_dict[my_friend].append(friend_x)
+                        else:
+                            suggested_friends_dict[my_friend] = [friend_x]
+                        if friend_x not in suggested_friends_dict.keys():
+                            suggested_friends_dict[friend_x] = []
+            suggested_friends_graph = Graph(suggested_friends_dict)
+            set_suggested_friends = set(suggested_friends_graph.bfs())
+            set_friends = set(friends)
+            set_friends.add(User.objects.get(username=curr_user))
+            filtered_friends = set_suggested_friends.difference(set_friends)
+            context['suggested_friends'] = filtered_friends
+            context['friends'] = friends
+    if request.method == 'POST':
+        friend_username = request.POST['friend_username']
+        if User.objects.filter(username=friend_username):
+            friend_list.add_friend(User.objects.get(username=friend_username))
+            return render(request, "account.html", context)
+        else:
+            message = 'Username entered does not exist'
+            context['error'] = message
+    return render(request, "account.html", context)
 
 
 def edit_account(request):
