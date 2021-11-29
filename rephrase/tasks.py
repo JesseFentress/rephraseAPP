3528d92a98420838b79394ee19cc85613a90c05f
JesseFentress <jessefentress246@gmail.com>
2021-11-29 18:53:15 -0500

    chat fixing

--- rephrase/tasks.py
+++ rephrase/tasks.py
@@ -1,5 +1,6 @@
 from celery import shared_task
 import requests
+from .models import Chat, User, Message
 
 
 @shared_task
@@ -19,3 +20,22 @@ def translate_message(request, message):
     print(response.text)
     text = response.json()
     return text['data']['translation']
+
+@shared_task
+def message_sessions(request):
+    new_messages = []
+    new_ids = []
+    for chat in Chat.objects.filter(user=User.objects.get(id=request.session['_auth_user_id'])):
+        if ('chat_messages_' + str(chat.id)) not in request.session.keys():
+            request.session['chat_messages_' + str(chat.id)] = []
+        if ('message_ids_' + str(chat.id)) not in request.session.keys():
+            request.session['message_ids_' + str(chat.id)] = []
+        if Message.objects.filter(chat_id=chat.id):
+            for messages in Message.objects.filter(chat_id=chat.id):
+                if messages.id not in request.session['message_ids_' + str(chat.id)]:
+                    new_messages.append(translate_message(request, messages.text))
+                    new_ids.append(messages.id)
+                    request.session['chat_messages_' + str(chat.id)].append(translate_message(request, messages.text))
+                    request.session['message_ids_' + str(chat.id)].append(messages.id)
+                else:
+                    pass
--- rephrase/views.py
+++ rephrase/views.py
@@ -7,7 +7,7 @@ from django.http import HttpResponse, JsonResponse
 from rephrase.forms import UserRegistrationForm, EditUserForm, CreateChatForm
 from rephrase.models import Chat, Message, User, UserChat
 from rephrase.Graph import Graph
-from .tasks import translate_message
+from .tasks import translate_message, message_sessions
 import requests
 
 
@@ -19,11 +19,11 @@ from rephrase.models import FriendsList, User
 def home(request):
     context = {}
     user = request.user
-    #del request.session['message_ids_2']
-    #del request.session['message_ids_26']
+    # del request.session['message_ids_2']
+    # del request.session['message_ids_26']
     #del request.session['message_ids_1']
-    #del request.session['chat_messages_2']
-    #del request.session['chat_messages_26']
+    # del request.session['chat_messages_2']
+    # del request.session['chat_messages_26']
     #del request.session['chat_messages_1']
 
     if user.is_authenticated:
@@ -34,25 +34,6 @@ def home(request):
             context['c'].append('{} => {}'.format(key, value))
     return render(request, 'home.html', context)
 
-
-def message_sessions(request):
-    new_messages = []
-    new_ids = []
-    for chat in Chat.objects.filter(user=User.objects.get(id=request.session['_auth_user_id'])):
-        if ('chat_messages_' + str(chat.id)) not in request.session:
-            request.session['chat_messages_' + str(chat.id)] = []
-        for messages in Message.objects.filter(chat_id=chat.id):
-            if ('message_ids_' + str(chat.id)) not in request.session:
-                request.session['message_ids_' + str(chat.id)] = []
-            if messages.id not in request.session['message_ids_' + str(chat.id)]:
-                new_messages.append(translate_message(request, messages.text))
-                new_ids.append(messages.id)
-            else:
-                pass
-            l1 = list(request.session['chat_messages_' + str(chat.id)])
-            request.session['chat_messages_' + str(chat.id)] = l1 + new_messages
-            l2 = list(request.session['message_ids_' + str(chat.id)])
-            request.session['message_ids_' + str(chat.id)] = l2 + new_ids
 
 
 def sign_up(request):
@@ -203,6 +184,7 @@ def chat(request, chat_id):
             chat_name = form.cleaned_data['name']
             form.save()
             Chat.objects.get(name=chat_name).user.add(current_user)
+            return redirect('chat', chat_id)
         else:
             context['create_chat_form'] = create_chat_form
 
@@ -215,42 +197,22 @@ def send(request):
     chat_id = request.POST['chat_id']
     if len(message.strip()) > 0:
         new_message = Message.objects.create(text=message, user=User.objects.get(username=username), chat=Chat.objects.get(id=chat_id))
-        new_message.save(commit=False)
-        new_id = new_message.id
         new_message.save()
-        l1 = list(request.session['chat_messages_' + str(chat.id)])
-        request.session['chat_messages_' + str(chat.id)] = l1 + [translate_message(request, message)]
-        l2 = list(request.session['message_ids_' + str(chat.id)])
-        request.session['message_ids_' + str(chat.id)] = l2 + [new_id]
     return HttpResponse('Message sent successfully')
 
 
 def getMessages(request, chat_id):
     chat_details = Chat.objects.get(id=chat_id)
     messages = Message.objects.filter(chat=chat_details)
-    message_sessions(request)
-    users = []
-    date_time = []
-    for message in messages:
-        users.append(message.user.username)
-        date_time.append(message.date_time)
-        tran_msg = request.session['chat_messages_' + str(chat_id)]
+    if len(list(messages)) > 0:
+        message_sessions(request)
+        users = []
+        date_time = []
+        for message in messages:
+            users.append(message.user.username)
+            date_time.append(message.date_time)
+            tran_msg = request.session['chat_messages_' + str(chat_id)]
+        return JsonResponse({'messages': list(tran_msg), 'users': users, 'date_time': date_time})
+    else:
+        return JsonResponse({'messages': [], 'users': [], 'date_time': []})
 
-    return JsonResponse({'messages': list(tran_msg), 'users': users, 'date_time': date_time})
-
-
-#def translate_message(request, message):
- #   url = "https://google-translate20.p.rapidapi.com/translate"
-
-  #  querystring = {"text": message, "tl": request.session['language'], "sl": "en"}
-
-   # headers = {
-    #    'x-rapidapi-host': "google-translate20.p.rapidapi.com",
-     #   'x-rapidapi-key': "1bc48f3f83msh1aab1bdcb2f9ea9p102fdfjsn5799cd77b3d7"
-    #}
-
-    #response = requests.request("GET", url, headers=headers, params=querystring)
-
-    #print(response.text)
-    #text = response.json()
-    #return text['data']['translation']
